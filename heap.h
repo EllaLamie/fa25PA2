e3c5b699d593de941bddb23e2f4364669785b705
EllaLamie <elamie2017@sdsu.edu>
2025-11-03 00:28:58 -0800

    Finished all comments and debugging

--- heap.h
+++ heap.h
@@ -14,61 +14,114 @@ struct MinHeap {
 
     MinHeap() { size = 0; }
 
+    // Takes parameter idx to push into the array and weightArr[] as the array for upheap
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+
+        // Check to make sure we are not going over 64 which is almost impossible
+        if (size >= 64) {
+            cout<<"Heap overflow"<<endl;
+            return;
+        }
+
+        // Assigning index to the data array at position size
         data[size] = idx;
+
+        //Now setting pos to where ever size is
         int pos = size;
+
+        // Incrementing size
         size++;
+
+        // Using pos to upheap
         upheap(pos, weightArr);
     }
 
+    // Replaces root with last element, then calls downheap() to keep min heap property
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        if (size == 0) return -1; // placeholder
+
+        // Case if no items in the array
+        if (size == 0) return -1;
+
+        // Assign the first item in the array to the root
         int root = data[0];
+
+        // Now overwrite the root item by assigning it to the last element in the array
         data[0] = data[size - 1];
+
+        // Decreasing the size by 1
         size--;
+
+        // Downheap to maintain min heap order
         downheap(0, weightArr);
+
         return root;
     }
 
+    //Method to maintain min heap property using iterative traversal and comparisons
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+
+        // Loops until we have gone through all the elements
         while (pos > 0) {
+
+            // Finding the parent node of the current position
             int parentPos = (pos - 1) / 2;
+
+            // Comparing to see if the child or parent is less
             if (weightArr[data[pos]] < weightArr[data[parentPos]]) {
+
+                // Swapping the two if so by using a temp variable
                 int temp = data[pos];
                 data[pos] = data[parentPos];
                 data[parentPos] = temp;
+
+                // Now assigning the position to the parent to repeat the process
                 pos = parentPos;
             }
             else {
+
+                // If not, no swapping needed so break out of loop
                 break;
             }
         }
     }
 
+    // Swap parent down while larger than any child
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+
+        // Loops until break statement is reached
         while (true) {
+
+            // Finding left and right child nodes
             int leftNode = 2 * pos + 1;
             int rightNode = leftNode + 1;
+
+            // Assigning the smallest position to the current position temporarily
             int smallestPos = pos;
 
+            // Setting the smallest position to the left node if less than
             if (leftNode < size && weightArr[data[leftNode]] < weightArr[data[smallestPos]]) {
                 smallestPos = leftNode;
             }
+
+            // Setting the smallest position to the right node if less than
             if (rightNode < size && weightArr[data[rightNode]] < weightArr[data[smallestPos]]) {
                 smallestPos = rightNode;
             }
+
+            // If smallest position does not equal current position, swap them
             if (smallestPos != pos) {
+
+                // Using a temp to swap the data at the current and smallest positions
                 int temp = data[pos];
                 data[pos] = data[smallestPos];
                 data[smallestPos] = temp;
+
+                // Now setting the current position to the smallest position
                 pos = smallestPos;
             }
             else {
+
+                // Breaks out of loop if none of the above conditions were met
                 break;
             }
         }
--- main.cpp
+++ main.cpp
@@ -48,16 +48,23 @@ int main() {
     Function Definitions (Students will complete logic)
   ------------------------------------------------------*/
 
-// Step 1: Read file and count frequencies
+// Read file and count frequencies
 void buildFrequencyTable(int freq[], const string& filename) {
+
+    // Opening file to read from
     ifstream file(filename);
+
+    // Check to make sure file is able to open. If not, will exit program
     if (!file.is_open()) {
         cerr << "Error: could not open " << filename << "\n";
         exit(1);
     }
 
     char ch;
+
+    // While loop to read from the file until the end
     while (file.get(ch)) {
+
         // Convert uppercase to lowercase
         if (ch >= 'A' && ch <= 'Z')
             ch = ch - 'A' + 'a';
@@ -66,20 +73,37 @@ void buildFrequencyTable(int freq[], const string& filename) {
         if (ch >= 'a' && ch <= 'z')
             freq[ch - 'a']++;
     }
+
+    // Closing file after reading is done
     file.close();
 
     cout << "Frequency table built successfully.\n";
 }
 
-// Step 2: Create leaf nodes for each character
+// Using an iterative traversal to assign the characters to charArr, frequency to weightArr,
+// and the children of each to -1. This creates our leaf nodes
 int createLeafNodes(int freq[]) {
+
+    // Assigning the nextFree node to 0
     int nextFree = 0;
+
+    // Iterative traversal to find the number of leaf nodes
     for (int i = 0; i < 26; ++i) {
+
+        // Making sure we have items in the array
         if (freq[i] > 0) {
+
+            // Assigning the nextFreeth node to a letter using ASCII code
             charArr[nextFree] = 'a' + i;
+
+            // Now assign the frequency of an element to weightArray
             weightArr[nextFree] = freq[i];
+
+            // Now assign the indices to -1 because they have no children
             leftArr[nextFree] = -1;
             rightArr[nextFree] = -1;
+
+            // Move up nextFree to next free node
             nextFree++;
         }
     }
@@ -89,53 +113,78 @@ int createLeafNodes(int freq[]) {
 
 // Step 3: Build the encoding tree using heap operations
 int buildEncodingTree(int nextFree) {
-    // TODO:
-    // 1. Create a MinHeap object.
+
+    // Create a MinHeap object.
     MinHeap minHeap;
-    // 2. Push all leaf node indices into the heap.
+
+    // Push all leaf node indices into the heap.
     for (int i = 0; i < nextFree; i++) {
         minHeap.push(i, weightArr);
     }
-    // 3. While the heap size is greater than 1:
+
+    // While the heap size is greater than 1, use Huffman coding to find roots
+    // by continuously adding the two top nodes into one parent node and setting
+    // the two top nodes as the parent node's children. Then, when pushing the
+    // parent back into the heap, we get the Huffman coding tree
     while (minHeap.size > 1) {
-        //    - Pop two smallest nodes
+
+        // Pop two smallest nodes
         int first = minHeap.pop(weightArr);
         int second = minHeap.pop(weightArr);
-        //    - Create a new parent node with combined weight
+
+        // Create a new parent node with combined weight of first and second
         weightArr[nextFree] = weightArr[first] + weightArr[second];
-        //    - Set left/right pointers
+
+        // Set left/right pointers of the new parent node
         leftArr[nextFree] = first;
         rightArr[nextFree] = second;
-        //    - Push new parent index back into the heap
+
+        // Push new parent index back into the heap
         minHeap.push(nextFree, weightArr);
+
+        // Increment nextFree
         nextFree++;
-        // 4. Return the index of the last remaining node (root)
     }
+
+    // Return the index of the last remaining node (root)
     int root = minHeap.pop(weightArr);
     return root;
-    //return -1; // placeholder
 }
 
-// Step 4: Use an STL stack to generate codes
+// Use an STL stack to generate codes. We can use this to access the top and pop from there.
+// We also use a while loop to assign the info and position to a node. When we are in the
+// process of trying to reach a node, we add 0 for every left node and 1 for every right node
 void generateCodes(int root, string codes[]) {
-    // TODO:
+    // Make sure we have a heap
     if (root == -1) {
         return;
     }
+
     // Use stack<pair<int, string>> to simulate DFS traversal.
     stack<pair<int, string>> stack;
+
+    // Push the root of the tree into the stack
     stack.push({root, ""});
+
+    // While the stack is not empty, assign the info and position to the top and pop it
     while (!stack.empty()) {
+
+        // Assign the top of the stack to temp
         pair<int, string> temp = stack.top();
+
+        // Now assign the position and info to second and first
         int position = temp.first;
         string info = temp.second;
+
+        // Pop that from the stack
         stack.pop();
 
-        // Record code when a leaf node is reached.
+        // Record code (AKA info in my code) when a leaf node is reached
         if (leftArr[position] == -1 && rightArr[position] == -1) {
             codes[charArr[position] - 'a'] = info;
         }
         else {
+
             // Left edge adds '0', right edge adds '1'.
             if (rightArr[position] != -1) {
                 stack.push({rightArr[position], info + "1"});
@@ -147,24 +196,41 @@ void generateCodes(int root, string codes[]) {
     }
 }
 
-// Step 5: Print table and encoded message
+// Print table and encoded message
 void encodeMessage(const string& filename, string codes[]) {
     cout << "\nCharacter : Code\n";
+
+    // Loop for every letter in the alphabet
     for (int i = 0; i < 26; ++i) {
+
+        // Use codes to find if we have that letter
         if (!codes[i].empty())
+
+            // Use ASCII to find letter and print the code
             cout << char('a' + i) << " : " << codes[i] << "\n";
     }
 
     cout << "\nEncoded message:\n";
 
+    // Use file to find the characters
     ifstream file(filename);
     char ch;
+
+    // While loop for all the characters in the file
     while (file.get(ch)) {
+
+        // Convert uppercase to lowercase
         if (ch >= 'A' && ch <= 'Z')
             ch = ch - 'A' + 'a';
+
+        // If they are lowercase
         if (ch >= 'a' && ch <= 'z')
+
+            // Prints binary code for whichever letter
             cout << codes[ch - 'a'];
     }
     cout << "\n";
+
+    // Close the file after done reading
     file.close();
 }
